3b3bef461ff5021bfe84feba6d6933ee7b1b7044
enikebraimoh <richardbraimoh@gmail.com>
2021-09-16 17:31:19 +0100

    finished fragment_cctv.xml

--- app/src/main/java/com/trina/energy/ui/CctvFragment.kt
+++ app/src/main/java/com/trina/energy/ui/CctvFragment.kt
@@ -1,5 +1,7 @@
 package com.trina.energy.ui
 
+import android.view.animation.AnimationUtils
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.appbar.AppBarLayout
 import com.trina.energy.R
 import com.trina.energy.core.BaseFragment
@@ -10,6 +12,13 @@ class CctvFragment : BaseFragment<FragmentCctvBinding>(R.layout.fragment_cctv){
     override fun start() {
         super.start()
         coordinateMotion()
+        val animation = AnimationUtils.loadAnimation(requireContext(), R.anim.bounce)
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.callForYours.setOnClickListener {
+            binding.callForYours.startAnimation(animation)
+        }
     }
 
     private fun coordinateMotion() {
